f72f1dbecce67164bb3bc1f7f3c5c43df28eb1e8
OlekSandr-offensive <oleksandr.tarasyuk92@gmail.com>
2024-01-24 19:20:04 +0100

    Updates

--- _nuxt/index.0e06db79.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,o as c,a as s}from"./entry.5ac35f97.js";const t={},n={class:"container"},a=s("h1",null,"Home page",-1),_=[a];function r(d,i){return c(),o("div",n,_)}const f=e(t,[["render",r]]);export{f as default};
